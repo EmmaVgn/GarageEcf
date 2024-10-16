9fadb8f0d614be936c52c1f9dc698cb4927f5df1
EmmaVgn <emma.vigneron@outlook.fr>
2024-10-16 14:55:53 +0200

    add app.js

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,12 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import "./styles/app.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import 'nouislider/dist/nouislider.css';
+import 'nouislider';
+import 'flip-toolkit';
+
 
 require("bootstrap");
 // Range Slider
